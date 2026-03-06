0e6a8065e225e4a21a9cebc6fc5f742bdc086e30
Madhesh <145985492+Madhesh13@users.noreply.github.com>
2026-03-06 10:38:50 +0530

    Added Heading

--- manage.py
+++ manage.py
@@ -5,6 +5,7 @@ import sys
 
 
 def main():
+    """main file"""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
     try:
         from django.core.management import execute_from_command_line
