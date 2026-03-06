a94d4da7422613b53bd188306b7e9235642c9b9e
Madhesh <145985492+Madhesh13@users.noreply.github.com>
2026-03-06 10:39:47 +0530

    new

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    """main file"""
+    """new main file"""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
     try:
         from django.core.management import execute_from_command_line
